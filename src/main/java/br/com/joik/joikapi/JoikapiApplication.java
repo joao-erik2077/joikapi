79c945464c9242f05741f55e0cdb4dfcdef0adca
João Érik da Silva Crisóstomo <joaoerik2074@gmail.com>
2022-08-01 08:52:23 -0300

    Initial commit

---
+++ src/main/java/br/com/joik/joikapi/JoikapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.joik.joikapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoikapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoikapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/joik/joikapi/JoikapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joik.joikapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoikapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
