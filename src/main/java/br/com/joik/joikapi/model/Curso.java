7b32219cb3e3d5381d8d70b18266982b13cdf1b7
João Érik da Silva Crisóstomo <joaoerik2074@gmail.com>
2022-08-09 09:30:45 -0300

    Created Curso class

---
+++ src/main/java/br/com/joik/joikapi/model/Curso.java
@@ -0,0 +1,22 @@
+package br.com.joik.joikapi.model;
+
+public class Curso {
+    private Long id;
+    private String nomecurso;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomecurso() {
+        return nomecurso;
+    }
+
+    public void setNomecurso(String nomecurso) {
+        this.nomecurso = nomecurso;
+    }
+}
