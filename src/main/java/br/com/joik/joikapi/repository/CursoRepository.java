6ea41b5ea86ef564e247a6a8568acb39c795a771
João Érik da Silva Crisóstomo <joaoerik2074@gmail.com>
2022-08-30 09:38:29 -0300

    Add JPARepository

--- src/main/java/br/com/joik/joikapi/repository/CursoRepository.java
+++ src/main/java/br/com/joik/joikapi/repository/CursoRepository.java
@@ -1,4 +1,9 @@
 package br.com.joik.joikapi.repository;
 
-public interface CursoRepository {
+import br.com.joik.joikapi.model.Curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CursoRepository extends JpaRepository<Curso, Long> {
 }
