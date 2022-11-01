4c42709c78369f6a72b4138984e6c9215b57b966
João Érik da Silva Crisóstomo <joaoerik2074@gmail.com>
2022-11-01 09:44:50 -0300

    Get all cursos method

--- src/main/java/br/com/joik/joikapi/resource/CursoResource.java
+++ src/main/java/br/com/joik/joikapi/resource/CursoResource.java
@@ -1,7 +1,22 @@
 package br.com.joik.joikapi.resource;
 
+import br.com.joik.joikapi.model.Curso;
+import br.com.joik.joikapi.repository.CursoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("cursos")
 public class CursoResource {
+
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    public List<Curso> listarTodosCursos() {
+        return null;
+    }
+
 }
